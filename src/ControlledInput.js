23c4d1a3de8cfb855d3672f40b9fe959d91ac45b
Nahid Guliyev <naguliyev2018@ada.edu.az>
2022-12-24 12:50:15 +0100

    Initial commit

---
+++ src/ControlledInput.js
@@ -0,0 +1,31 @@
+import { useRef, useState, useEffect } from 'react'
+
+export const ControlledInput = (props) => {
+  const { value, onChange, ...rest } = props
+  const [cursor, setCursor] = useState(null)
+  const ref = useRef(null)
+  // useEffect(() => {
+  //   const input = ref.current
+  //   if (input) input.setSelectionRange(cursor, cursor)
+  // }, [ref, cursor, value])
+  // const handleChange = (e) => {
+  //   setCursor(e.target.selectionStart)
+  //   onChange && onChange(e)
+  // }
+  // return <input ref={ref} value={value} onChange={handleChange} {...rest} />
+  return (
+    <>
+      <button style={{ width: '100px' }} ref={ref} {...rest}>
+        Location
+      </button>
+      <br />
+      <button style={{ width: '100px' }} ref={ref} {...rest}>
+        Phone
+      </button>
+      <br />
+      <button style={{ width: '100px' }} ref={ref} {...rest}>
+        Email
+      </button>
+    </>
+  )
+}
